eb6422c0b957c7e31949042f183063b898ace743
SalvadorRF <54685749+SalvadorRF@users.noreply.github.com>
2022-11-30 00:58:34 -0600

    Title

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -7,6 +7,7 @@ function Home() {
     <>
     <div className="container">
       <img src="https://files.adventistas.org/apocalipsis.org/2019/01/banner_desktop-640x360.jpg" alt="" width="100%"/>
+      <h1 className='text-center'>Bienvenido a la mejor plataforma de Enseñanza del Apocalipsis!</h1>
     </div>
     <CoursesList/>
     </>
